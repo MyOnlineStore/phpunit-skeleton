6a58a90a3ff9c57cd518f569eb78838b8b22bb2d
Martin Beukman <martbeuk@gmail.com>
2018-06-15 08:28:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# phpunit-skeleton
+tool to create phpunit skeleton for files
+
+Generates een skeleton file for your php file. Includes mocks for all constructor dependencies and initialzes the class 
+with these dependencies.
+
+usage:
+
+`./phpunit-skeleton.phar create [filename]`
+
+building a new version of the phar (with box)
+`./vendor/bin/box build`
---
+++ src/Console/Command/CreateUnitTest.php
@@ -0,0 +1,191 @@
+<?php
+declare(strict_types=1);
+
+namespace MyOnlineStore\PhpUnitSkeleton\Console\Command;
+
+use PhpParser\Node\Name;
+use PhpParser\Node\Stmt\Use_;
+use Roave\BetterReflection\BetterReflection;
+use Roave\BetterReflection\Reflector\ClassReflector;
+use Roave\BetterReflection\SourceLocator\Type\SingleFileSourceLocator;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+final class CreateUnitTest extends Command
+{
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this->setName('create')
+            ->setDescription('Create a skeleton phpunit class with all constructor dependencies as mock')
+            ->addArgument('filename', InputArgument::REQUIRED, 'input filename (relative path');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $filePath = $input->getArgument('filename');
+        $helper = $this->getHelper('question');
+        $newFileName = null;
+
+        if (false !== ($position = strpos($filePath, 'Application'))) {
+            $newFileName = substr_replace($filePath, '/Tests', $position + 11, 0);
+        } elseif (false !== ($position = strpos($filePath, 'Domain'))) {
+            $newFileName = substr_replace($filePath, '/Tests', $position + 6, 0);
+        } elseif (false !== ($position = strpos($filePath, 'Infrastructure'))) {
+            $newFileName = substr_replace($filePath, '/Tests', $position + 14, 0);
+        }
+
+        if (null === $newFileName) {
+            throw new \InvalidArgumentException('no valid filename provided');
+        }
+
+        $somePieces = explode('.', $newFileName);
+        $newFileName = sprintf('%sTest.%s', $somePieces[0], $somePieces[1]);
+
+        if (\file_exists($newFileName)) {
+            $question = new ConfirmationQuestion('existing unittest will be overwrite is this ok?', false);
+        } else {
+            $question = new ConfirmationQuestion(
+                sprintf('Unit test will be created at %s is this ok?', $newFileName),
+                false
+            );
+        }
+
+        if (!$helper->ask($input, $output, $question)) {
+            return;
+        }
+
+        $astLocator = (new BetterReflection())->astLocator();
+        $reflector = new ClassReflector(new SingleFileSourceLocator($filePath, $astLocator));
+
+        $sourceClass = $reflector->getAllClasses()[0];
+
+        $namespace = null;
+        $className = $sourceClass->getShortName();
+        $properties = $sourceClass->getImmediateProperties();
+        $newNameSpace = null;
+        $imports = [];
+
+        foreach ($sourceClass->getDeclaringNamespaceAst() as $nodes) {
+            if ($nodes instanceof Name) {
+                $namespace = $nodes->parts;
+
+                break;
+            }
+        }
+
+        foreach ($sourceClass->getDeclaringNamespaceAst()->stmts as $statement) {
+            if (!$statement instanceof Use_) {
+                continue;
+            }
+
+            $imports[] = implode('\\', $statement->uses[0]->name->parts);
+        }
+
+        if (null === $namespace) {
+            throw new \InvalidArgumentException('no valid namespace provided');
+        }
+
+        $lastPartOfNameSpace = $namespace;
+        unset($lastPartOfNameSpace[0], $lastPartOfNameSpace[1]);
+        $newNameSpace = implode('\\', array_merge([$namespace[0], $namespace[1], 'Tests'], $lastPartOfNameSpace));
+
+        if (empty($newNameSpace)) {
+            throw new \InvalidArgumentException('no valid namespace provided');
+        }
+
+        $template = sprintf("<?php\ndeclare(strict_types=1);\n\nnamespace %s;\n\n", $newNameSpace);
+        $template .= sprintf("use %s;\n", $sourceClass->getName());
+
+        foreach ($imports as $import) {
+            $template .= sprintf("use %s;\n", $import);
+        }
+
+        $template .= sprintf(
+            "\nfinal class %sTest extends \PHPUnit\Framework\TestCase\n{\n",
+            $sourceClass->getShortName()
+        );
+
+        foreach ($properties as $property) {
+            $propertyName = 'public';
+
+            if ($property->isPrivate()) {
+                $propertyName = 'private';
+            }
+
+            if ($property->isProtected()) {
+                $propertyName = 'protected';
+            }
+
+            $template .= sprintf(
+                "    %s\n    %s $%s;\n\n",
+                $property->getDocComment(),
+                $propertyName,
+                $property->getName()
+            );
+        }
+
+        $template .= sprintf("    /**\n    * @var %s\n    */\n    private \$objectToUse;\n\n", $className);
+        $template .= "    protected function setUp()\n    {\n";
+
+        foreach ($properties as $property) {
+            if (preg_match('/@var\s+([^\s]+)/', $property->getDocComment(), $matches)) {
+                list(, $type) = $matches;
+                $template .= sprintf(
+                    '        $this->%s = $this->createMock(%s::class);%s',
+                    $property->getName(),
+                    $type,
+                    "\n"
+                );
+            }
+        }
+
+        $propertiesCount = count($properties);
+
+        if (0 !== $propertiesCount) {
+            $template .= "\n";
+        }
+
+        $template .= sprintf("        \$this->objectToUse = new %s(", $className);
+
+        if (0 === $propertiesCount) {
+            $template .= ');';
+        } else {
+            $iteration = 1;
+            $template .= "\n";
+
+            foreach ($properties as $property) {
+                $template .= sprintf('            $this->%s', $property->getName());
+
+                if ($propertiesCount !== $iteration) {
+                    $template .= ",\n";
+                }
+
+                $iteration++;
+            }
+
+            $template .= "\n        );";
+        }
+
+        $template .= "\n    }\n}";
+
+        $dirName = \dirname($newFileName);
+
+        if (!is_dir($dirName)) {
+            mkdir($dirName, 0777, true);
+            chown($dirName, 1000);
+        }
+
+        $newFile = fopen($newFileName, 'wb');
+        fwrite($newFile, $template);
+        fclose($newFile);
+    }
+}
